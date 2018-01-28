b63432192ac5cb6ed91a6a7f668c02a50d78c77c
Davide Masserut <d.masserut@gmail.com>
2018-01-28 20:38:08 +0100

    Add pids_supported()

--- yacm/obd_io.py
+++ yacm/obd_io.py
@@ -59,3 +59,34 @@ class ObdIO(object):
                 result = "NO DATA"
         # print(result)
         return result
+
+    def pids_supported(self):
+        hex2bin_map = {
+            "0": "0000",
+            "1": "0001",
+            "2": "0010",
+            "3": "0011",
+            "4": "0100",
+            "5": "0101",
+            "6": "0110",
+            "7": "0111",
+            "8": "1000",
+            "9": "1001",
+            "A": "1010",
+            "B": "1011",
+            "C": "1100",
+            "D": "1101",
+            "E": "1110",
+            "F": "1111",
+        }
+        pids_supported = []
+        for pid in [00, 20, 40, 60, 80]:
+            pids = ''.join(self.query("01", pid))
+            if pids != "?":
+                binary_pids = ''.join(hex2bin_map[nibble] for nibble in pids)
+                pid_code = pid
+                for bit in binary_pids:
+                    pid_code += 1
+                    if bit == "1":
+                        pids_supported.append(hex(pid_code)[2:])
+        return pids_supported
