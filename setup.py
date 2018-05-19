e2a8bcd3c7d87babbd54a90451bb259868f2cb6c
Davide Masserut <d.masserut@gmail.com>
2018-05-19 15:49:50 +0200

    Rename yacm to yacmt

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='yacm',
+    name='yacmt',
     version='0.1',
     author='Davide Masserut',
     author_email='d.masserut@gmail.com',
-    packages=['yacm'],
+    packages=['yacmt'],
     install_requires=['pyserial', 'click', 'filelock'],
     extras_require={'dev': ['line_profiler', 'pylint']},
-    scripts=['bin/yacm_bluetooth'],
-    entry_points={'console_scripts': ['yacm = yacm.yacm_cli:main']})
+    scripts=['bin/yacmt_bluetooth'],
+    entry_points={'console_scripts': ['yacmt = yacmt.yacmt_cli:main']})
