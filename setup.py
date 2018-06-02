033fb13e55837349133977b8610012dac5e6ab32
Davide Masserut <d.masserut@gmail.com>
2018-06-02 17:45:45 +0200

    Fix module name

--- setup.py
+++ setup.py
@@ -9,4 +9,4 @@ setup(
     install_requires=['pyserial', 'click', 'filelock'],
     extras_require={'dev': ['line_profiler', 'pylint']},
     scripts=['bin/yacmt_bluetooth'],
-    entry_points={'console_scripts': ['yacmt = yacmt.yacmt_cli:main']})
+    entry_points={'console_scripts': ['yacmt = yacmt.yacmt:main']})
